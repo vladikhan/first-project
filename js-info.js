754141ac465b726cb3a552961059f8e1fc07b32a
vladikhan <84037014+vladikhan@users.noreply.github.com>
2022-07-07 00:29:44 +0900

    Delete js-info.js

--- js-info.js
+++
@@ -1,5 +0,0 @@
-alert("Брендан Эйх");
-alert('JavaScript был создан за 10 дней');
-alert("JavaScript был создан в 1995 году");
-alert("JavaScript - это не Java");
-alert("Спецификация (формальное описание синтаксиса, базовых объектов и алгоритмов) языка JavaScript называется ECMAScript.")
